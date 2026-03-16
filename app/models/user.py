4104385cddb8a106ef27c9cfc6dcb595b32f8555
NikosKl <nikos.klavi@gmail.com>
2026-03-16 21:01:03 +0200

    feat: add user model

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import uuid
+from sqlalchemy import UUID, String, text, Boolean, func, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email: Mapped[str] = mapped_column(String(255), index=True, unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(255), index=True, unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_name: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, server_default=text('true'), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.core.config import settings
 from alembic import context
+import app.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
