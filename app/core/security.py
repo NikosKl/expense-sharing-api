0a251bf11d4a0931bea285e342eec1a6e11c8aa8
NikosKl <nikos.klavi@gmail.com>
2026-03-17 19:01:49 +0200

    feat: add create and decode token helpers

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,7 @@
+from datetime import timedelta, timezone, datetime
+import jwt
+from jwt import InvalidTokenError
+from app.core.config import settings
 from pwdlib import PasswordHash
 
 password_hash = PasswordHash.recommended()
@@ -7,3 +11,26 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return password_hash.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
+    return encoded_jwt
+
+def decode_token(token: str) -> dict:
+    payload = jwt.decode(
+                token,
+                settings.jwt_secret_key,
+                algorithms=[settings.jwt_algorithm])
+
+    if not payload.get('sub'):
+        raise InvalidTokenError('Token missing subject')
+    return payload
