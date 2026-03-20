fc7cd1a4e9577dae5e749b9a624ac5148722fdcb
NikosKl <nikos.klavi@gmail.com>
2026-03-20 17:18:51 +0200

    feat: add group services

---
+++ app/services/group_service.py
@@ -0,0 +1,28 @@
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+from app.models import Group, User, GroupMember
+from app.schemas.group import GroupCreateRequest
+
+def create_group(db: Session, current_user: User, group_data: GroupCreateRequest) -> Group:
+    group = Group(
+        name=group_data.name,
+        description=group_data.description,
+        created_by=current_user.id
+    )
+
+    db.add(group)
+    db.flush()
+
+    group_member = GroupMember(
+        group_id=group.id,
+        user_id=current_user.id,
+        role='owner'
+    )
+    try:
+        db.add(group_member)
+        db.commit()
+        db.refresh(group)
+        return group
+    except IntegrityError as err:
+        db.rollback()
+        raise err
