0069136e6ae0d72cc16be0fd0aaf4e81d7b1cae8
NikosKl <nikos.klavi@gmail.com>
2026-03-21 17:31:45 +0200

    feat: add group member services

---
+++ app/services/group_member_service.py
@@ -0,0 +1,53 @@
+import uuid
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+from app.models import GroupMember, User
+from app.services.group_service import get_group_by_id
+from app.services.user_service import get_user_by_id
+
+class GroupNotFound(Exception):
+    pass
+class PermissionDeniedError(Exception):
+    pass
+class UserNotFound(Exception):
+    pass
+class GroupMemberAlreadyExists(Exception):
+    pass
+
+def add_member_to_group(db: Session, current_user: User, group_id: uuid.UUID, user_to_add: uuid.UUID) -> GroupMember:
+
+    group = get_group_by_id(db, current_user, group_id)
+    if group is None:
+        raise GroupNotFound()
+    current_member = get_group_member(db, group_id, current_user.id)
+    if current_member is None:
+        raise PermissionDeniedError()
+    if current_member.role != 'owner':
+        raise PermissionDeniedError()
+    user = get_user_by_id(db, user_to_add)
+    if user is None:
+        raise UserNotFound()
+    existing_member = get_group_member(db, group_id, user_to_add)
+    if existing_member is not None:
+        raise GroupMemberAlreadyExists()
+
+    group_member = GroupMember(
+        group_id=group_id,
+        user_id=user_to_add,
+        role="member"
+    )
+
+    db.add(group_member)
+    try:
+        db.commit()
+        db.refresh(group_member)
+        return group_member
+    except IntegrityError:
+        db.rollback()
+        raise
+
+def get_group_member(db: Session, group_id: uuid.UUID, user_id: uuid.UUID) -> GroupMember | None:
+    stmt = select(GroupMember).where(GroupMember.group_id == group_id, GroupMember.user_id == user_id)
+    group_member = db.scalar(stmt)
+    return group_member
