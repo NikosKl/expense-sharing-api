218d3c19c0c1e0744cf8e64eb79b6fa224bc940e
NikosKl <nikos.klavi@gmail.com>
2026-04-16 22:30:55 +0300

    test: strengthen test_get_all_groups

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -1,5 +1,5 @@
 import uuid
-from tests.conftest import get_auth_headers
+from tests.helpers import get_auth_headers
 
 def test_create_group(client):
 
@@ -16,34 +16,44 @@ def test_create_group(client):
     assert data['description'] == 'test description'
 
 def test_get_all_groups(client):
-    headers_one = get_auth_headers(
+    headers_a = get_auth_headers(
         client,
-        email='test_user_one@example.com',
-        username='test_user_one',
+        email='test_user_a@example.com',
+        username='test_user_a',
         password='long_password'
     )
-    response = client.post('/groups', json={'name': 'test_group_1'}, headers=headers_one)
+    response = client.post('/groups', json={'name': 'test_group_1'}, headers=headers_a)
     assert response.status_code == 200
+    group_a = response.json()
+    group_a_id = group_a['id']
 
-    headers_two = get_auth_headers(
+    headers_b = get_auth_headers(
         client,
-        email='test_user_two@example.com',
-        username='test_user_two',
+        email='test_user_b@example.com',
+        username='test_user_b',
         password='long_password'
     )
 
-    response = client.post('/groups', json={'name': 'test_group_2'}, headers=headers_two)
+    response = client.post('/groups', json={'name': 'test_group_2'}, headers=headers_b)
     assert response.status_code == 200
+    group_one = response.json()
+    group_one_id = group_one['id']
 
-    response = client.post('/groups', json={'name': 'test_group_3'}, headers=headers_two)
+    response = client.post('/groups', json={'name': 'test_group_3'}, headers=headers_b)
     assert response.status_code == 200
+    group_two = response.json()
+    group_two_id = group_two['id']
 
-    response = client.get('/groups', headers=headers_two)
+    response = client.get('/groups', headers=headers_b)
     assert response.status_code == 200
 
     data = response.json()
     assert isinstance(data, list)
     assert len(data) == 2
+    returned_ids = [group['id'] for group in data]
+    assert group_a_id not in returned_ids
+    assert group_one_id in returned_ids
+    assert group_two_id in returned_ids
 
 def test_get_group(client):
     headers = get_auth_headers(client)
