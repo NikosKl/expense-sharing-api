bc08ef2678fa9ad88f08bef14f282fb414154611
NikosKl <nikos.klavi@gmail.com>
2026-04-22 16:15:13 +0300

    feat: add GET group expenses api

--- app/api/expenses.py
+++ app/api/expenses.py
@@ -1,4 +1,6 @@
 import uuid
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.api.deps import get_current_user
@@ -7,7 +9,7 @@ from app.models import User
 from app.schemas.expense import ExpenseResponse, ExpenseCreateRequest
 from app.services.exceptions import GroupNotFound, PermissionDeniedError, InvalidPayerError, \
     InvalidParticipantsError
-from app.services.expense_service import create_expense
+from app.services.expense_service import create_expense, get_group_expenses
 
 router = APIRouter(prefix="/groups", tags=["groups"])
 
@@ -24,3 +26,13 @@ def create_new_expense(group_id: uuid.UUID, expense_request: ExpenseCreateReques
         raise HTTPException(status_code=400, detail="Payer must be a member of the group")
     except InvalidParticipantsError:
         raise HTTPException(status_code=400, detail="All participants must be member of the group")
+
+@router.get('/{group_id}/expenses', response_model=List[ExpenseResponse])
+def get_all_group_expenses(group_id: uuid.UUID, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    try:
+        expenses = get_group_expenses(db, current_user, group_id)
+        return expenses
+    except GroupNotFound:
+        raise HTTPException(status_code=404, detail="Group not found")
+    except PermissionDeniedError:
+        raise HTTPException(status_code=403, detail="You do not have permission to perform this action")
