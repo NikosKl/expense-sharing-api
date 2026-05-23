6e9f96402a9f6f9ae3d871cfb89dfa9d1ebd927f
NikosKl <nikos.klavi@gmail.com>
2026-05-23 16:09:59 +0300

    feat: add expense update endpoint

--- app/api/expenses.py
+++ app/api/expenses.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from app.api.deps import get_current_user
 from app.db.session import get_db
 from app.models import User
-from app.schemas.expense import ExpenseResponse, ExpenseCreateRequest
+from app.schemas.expense import ExpenseResponse, ExpenseCreateRequest, ExpenseUpdateRequest
 from app.services.exceptions import ExpenseNotFound, PermissionDeniedError, InvalidPayerError, InvalidParticipantsError, \
     InvalidExpenseSplitError
 from app.services.expense_service import get_expense_by_id, update_expense
@@ -22,8 +22,8 @@ def get_expense(expense_id: uuid.UUID, db: Session = Depends(get_db), current_us
     except PermissionDeniedError:
         raise HTTPException(status_code=403, detail="You do not have permission to perform this action")
 
-@router.put('/{expense_id}', response_model=ExpenseResponse)
-def update_existing_expense(expense_id: uuid.UUID, expense_data: ExpenseCreateRequest, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+@router.patch('/{expense_id}', response_model=ExpenseResponse)
+def update_existing_expense(expense_id: uuid.UUID, expense_data: ExpenseUpdateRequest, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     try:
         updated_expense = update_expense(db, current_user, expense_id, expense_data)
         return updated_expense
@@ -34,6 +34,6 @@ def update_existing_expense(expense_id: uuid.UUID, expense_data: ExpenseCreateRe
     except InvalidPayerError:
         raise HTTPException(status_code=400, detail='Payer must be a member of the group')
     except InvalidParticipantsError:
-        raise HTTPException(status_code=400, detail="All participants must be member of the group")
+        raise HTTPException(status_code=400, detail="All participants must be members of the group")
     except InvalidExpenseSplitError:
         raise HTTPException(status_code=400, detail="Splits must sum up to total amount")
