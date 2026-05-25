da1f801cee6006d0956d79cf648e43d8ad0e72c5
NikosKl <nikos.klavi@gmail.com>
2026-05-25 17:53:10 +0300

    feat: add expense delete endpoint

--- app/api/expenses.py
+++ app/api/expenses.py
@@ -1,14 +1,14 @@
 import uuid
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 from app.api.deps import get_current_user
 from app.db.session import get_db
 from app.models import User
-from app.schemas.expense import ExpenseResponse, ExpenseCreateRequest, ExpenseUpdateRequest
+from app.schemas.expense import ExpenseResponse, ExpenseUpdateRequest
 from app.services.exceptions import ExpenseNotFound, PermissionDeniedError, InvalidPayerError, InvalidParticipantsError, \
     InvalidExpenseSplitError
-from app.services.expense_service import get_expense_by_id, update_expense
+from app.services.expense_service import get_expense_by_id, update_expense, delete_expense
 
 router = APIRouter(prefix="/expenses", tags=["expenses"])
 
@@ -37,3 +37,12 @@ def update_existing_expense(expense_id: uuid.UUID, expense_data: ExpenseUpdateRe
         raise HTTPException(status_code=400, detail="All participants must be members of the group")
     except InvalidExpenseSplitError:
         raise HTTPException(status_code=400, detail="Splits must sum up to total amount")
+
+@router.delete('/{expense_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_existing_expense(expense_id: uuid.UUID, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    try:
+        delete_expense(db, current_user, expense_id)
+    except ExpenseNotFound:
+        raise HTTPException(status_code=404, detail="Expense not found")
+    except PermissionDeniedError:
+        raise HTTPException(status_code=403, detail="You do not have permission to perform this action")
